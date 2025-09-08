eaffbd25278c567e23aae9e1ca6614a9758178d8
rosendoalves <rosendoalves05@gmail.com>
2025-09-08 00:04:49 -0300

    last changes

--- src/components/Business.tsx
+++ src/components/Business.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 
 const Business: React.FC = () => {
   const businessServices = [
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
